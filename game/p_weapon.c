a3348e633d18e0bf163c46c634c8a3448dbe0613
wieczorekkevin <wieczorek.kevin.m@gmail.com>
2023-04-04 20:29:13 -0400

    created first mod folder for first weapon mod

--- game/p_weapon.c
+++ game/p_weapon.c
@@ -829,7 +829,7 @@ void Blaster_Fire (edict_t *ent, vec3_t g_offset, int damage, qboolean hyper, in
 	VectorScale (forward, -2, ent->client->kick_origin);
 	ent->client->kick_angles[0] = -1;
 
-	fire_blaster (ent, start, forward, damage, 1000, effect, hyper);
+	fire_rocket (ent, start, forward, 50, 5, 20, 10);
 
 	// send muzzle flash
 	gi.WriteByte (svc_muzzleflash);
